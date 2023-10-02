769f3d85d1e9aad3da6a345754d1f2b346f7cf62
Melo04 <kohhmelody@gmail.com>
2023-10-02 12:17:40 +0800

    fix: fix deployment error

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,7 +24,7 @@ import { FavouritesModule } from './favourites/favourites.module';
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      typePaths: ['./**/*.graphql'],
+      typePaths: ['./schema.graphql'],
       definitions: {
         path: join(process.cwd(), 'src/graphql.ts'),
         outputAs: 'class',
